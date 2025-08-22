963de19b2f203a6d1310ed7ae6a8178437f730c7
yago-vasco <yago.rc.04@gmail.com>
2025-08-22 12:34:40 +0200

    build(Query's): Query's de servicio
    Se han implementado 1 querys de servicio que posteriormente se llevaran a una query JPQL en la app

--- src/main/resources/static/wizards.sql
+++ src/main/resources/static/wizards.sql
@@ -1,5 +1,5 @@
 use datosdados;
-select * from spell_entity join user_entity on spell_entity.user_id= user_entity.id where user_entity.username = 'admin';
+select * from spell_entity join user_entity on spell_entity.user_id= user_entity.id where user_entity.username = 'admin' and spell_entity.name like 'A%';
 
 
 
